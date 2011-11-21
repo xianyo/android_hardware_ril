1bced20db8b3c31e06df51141dbe51c4433d0936
Xinyu Chen <b03824@freescale.com>
2011-11-21 08:57:37 +0800

    ENGR00000011 pass build on ICS
    Merge error and Callback struct version update
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- reference-ril/reference-ril.c
+++ reference-ril/reference-ril.c
@@ -529,8 +529,8 @@ static void requestDataCallList(void *data, size_t datalen, RIL_Token t)
 
 void onDeactiveDataCallList()
 {
-    RIL_Data_Call_Response *responses =
-        alloca(sizeof(RIL_Data_Call_Response));
+    RIL_Data_Call_Response_v4 *responses =
+        alloca(sizeof(RIL_Data_Call_Response_v4));
     LOGI("onDeactiveDataCallList");
     //For n pdp context, or just one pdp context?
     //Always cid =1 ?
@@ -548,7 +548,7 @@ void onDeactiveDataCallList()
 
     RIL_onUnsolicitedResponse(RIL_UNSOL_DATA_CALL_LIST_CHANGED,
                                   responses,
-                                  sizeof(RIL_Data_Call_Response));
+                                  sizeof(RIL_Data_Call_Response_v4));
 }
 
 static void requestOrSendDataCallList(RIL_Token *t)
@@ -1943,8 +1943,7 @@ static void requestSetupDataCall(void *data, size_t datalen, RIL_Token t)
 
     if(retry == 0)
 	goto ppp_error;
-#endif
-
+    }
     requestOrSendDataCallList(&t);
     at_response_free(p_response);
 
