f1e3078a1063ef30f88ed8055674c18277feef73
guoyin.chen <guoyin.chen@freescale.com>
2013-07-30 14:17:19 +0800

    ENGR00273070 Enable 3G data connection for JB4.3
    Make query_ctec return failure for HUAWEI&AMAZON modem
    Since they dont support it
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- reference-ril/reference-ril.c
+++ reference-ril/reference-ril.c
@@ -4008,7 +4008,7 @@ int query_ctec(ModemInfo *mdm __unused, int *current, int32_t *preferred)
 
     if ((HUAWEI_MODEM == modem_type) ||
             (AMAZON_MODEM == modem_type)){
-        return 0;
+        return -1;
     }else{
         RLOGD("query_ctec. current: %d, preferred: %d", (int)current, (int) preferred);
         err = at_send_command_singleline("AT+CTEC?", "+CTEC:", &response);
